141d7610711248cf846e2d7b09cd0ea36f492abc
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-28 08:03:21 +0200

    Update registerContainer.js

--- src/containers/registerContainer.js
+++ src/containers/registerContainer.js
@@ -1,58 +1,19 @@
-import React from 'react'
-import ContactForm from "../components/register/register"
+import React from 'react';
+import FormCode from '../components/register/register';
 
-class ContactPage extends React.Component {
-  constructor() {
-    super();
-  
-    this.state = {
-      
-      email: "",
-      password: "",
-      firstname:"",
-      secondname:"",
-    };
-   
+class Form extends React.Component {
+  submit(values) {
+    console.log(values);
   }
-
-  handleEmailChange = evt => {
-    this.setState({ email: evt.target.value });
-  };
-
-  handlePasswordChange = evt => {
-    this.setState({ password: evt.target.value });
-  };
-  validate = () => {
-    let nameError = "";
-    let emailError = "";
-    // let passwordError = "";
-
-    if (!this.state.name) {
-      nameError = "name cannot be blank";
-    }
-
-    if (!this.state.email.includes("@")) {
-      emailError = "invalid email";
-    }
-
-    if (emailError || nameError) {
-      this.setState({ emailError, nameError });
-      return false;
-    }
-
-    return true;
-  };
-  
   render() {
-    const { email, password } = this.state;
-    const isEnabled = email.length > 1 && password.length > 1;
-    return <ContactForm 
-    
-    onChangePassword={this.handlePasswordChange}
-    onChangeEmail={this.handleEmailChange}
-    password={this.state.password}
-    email={this.state.email}
-    isEnabled={isEnabled} />
+    return (
+      <div className="container">
+        <h3 className="form">Авторизация</h3>
+        <FormCode onSubmit={this.submit} />
+      </div>
+      
+    )
   }
 }
-export default ContactPage
+
+export default Form;
